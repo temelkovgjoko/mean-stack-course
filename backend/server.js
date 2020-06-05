2efa26f821ad166911346dd2bf1fea03eb9b3045
Gjoko <temelkovgjoko@gmail.com>
2020-06-05 15:00:26 +0200

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
-const app = require("./backend/app");
-const debug = require('debug')('node-angular')
+const app = require("./app");
+const debug = require("debug")("node-angular");
 const http = require("http");
-
 
 const normalizePort = (val) => {
   var port = parseInt(val, 10);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://nodeangular5-env.eba-66hpbt2p.eu-central-1.elasticbeanstalk.com/api"
 };
